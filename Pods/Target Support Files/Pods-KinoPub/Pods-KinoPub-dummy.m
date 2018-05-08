991f5102fe9f38c98cc1a2355e96bca7d65779db
Evgeny Dats <hintoz@gmail.com>
2018-05-08 20:13:06 +0300

    Combine with the qinoa application

--- Pods/Target Support Files/Pods-KinoPub/Pods-KinoPub-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KinoPub : NSObject
-@end
-@implementation PodsDummy_Pods_KinoPub
-@end
---
+++ Pods/Target Support Files/Pods-qinoa/Pods-qinoa-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qinoa : NSObject
+@end
+@implementation PodsDummy_Pods_qinoa
+@end
